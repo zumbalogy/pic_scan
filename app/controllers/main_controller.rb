c297b3eed7d97c090ebc594412b7aa3a77a32e6e
zumbalogy <intrapostal@gmail.com>
2014-02-10 22:09:29 -0500

    init

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+    def index
+    end
+    def copy
+    end
+    def test
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicScanRails::Application.config.secret_token = 'f4ad080c71229a7d2a2b6a03b0dad7ad1ba489866ecc80ddbd7d94f644617ec561d8f13a423cc68b9ec87478859c1d0f445469e37066cadb6e1f77c064c21525'
